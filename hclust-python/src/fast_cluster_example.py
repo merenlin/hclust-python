497f3f93d0dd5720b6d66d6330f26779e8819a64
OxanaSachenkova <merenlin@gmail.com>
2013-01-24 17:31:38 +0100

    make fastcluster example cleaner

--- hclust-python/src/fast_cluster_example.py
+++ hclust-python/src/fast_cluster_example.py
@@ -4,11 +4,8 @@ from fastcluster import *
 from hcluster import dendrogram 
 # Loading the data
 data = np.genfromtxt("../../data/ExpRawData-E-TABM-84-A-AFFY-44.tab",names=True,usecols=tuple(range(1,30)),dtype=float, delimiter="\t")
-print len(data)
+
 data_array = data.view((np.float, len(data.dtype.names)))
-print len(data_array)
-print len(data_array.transpose())
-#data_link = linkage(data_array.transpose(), method='single', metric='euclidean', preserve_input=True)
 data_link = linkage(data_array[1:1000], method='single', metric='euclidean', preserve_input=True)
 dendrogram(data_link)
 show()
