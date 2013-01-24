8b517e4d8e15ed69c8f942c72ad7dddd63e53351
OxanaSachenkova <merenlin@gmail.com>
2013-01-24 17:29:56 +0100

    some comments and links

--- README.md
+++ README.md
@@ -9,3 +9,34 @@ and represents typical data bioinformaticians work with.
 It is "Transcription profiling by array of brain in humans, chimpanzees and macaques,
 and brain, heart, kidney and liver in orangutans" in tab-separated format. 
 (http://www.ebi.ac.uk/gxa/experiment/E-TABM-84)
+
+
+***************** Clustering and ML in Python resources ***************** 
+
+Great material on clustering methods: 
+http://www-users.cs.umn.edu/~kumar/dmbook/dmslides/chap8_basic_cluster_analysis.pdf
+
+Summary of all algorithms for clustering (and other Unsupervised learning techniques that
+Python has to offer)
+http://scikit-learn.org/dev/unsupervised_learning.html 
+
+Approach №1: 
+
+Hclust using SciPy + Numpy packages
+
+Working with structured arrays: http://www.scipy.org/Cookbook/Recarray
+SciPy cluster: http://code.google.com/p/scipy-cluster/
+Linkage function: http://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+
+Which method to choose? 
+* ward's produces compact cluster
+* average linkage - doesn't scale well,
+
+shortcut: pylab! http://www.scipy.org/PyLab
+
+Approach №2:
+BioPython + TreeView
+
+
+Approach №3:
+fastcluster
--- hclust-python/src/sci_py_example.py
+++ hclust-python/src/sci_py_example.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pylab
-from hcluster import pdist, linkage, dendrogram,squareform
+from hcluster import pdist, linkage, dendrogram,squareform # same as import them from scipy
 
 data = np.genfromtxt("../../data/ExpRawData-E-TABM-84-A-AFFY-44.tab",names=True,usecols=tuple(range(1,30)),dtype=float, delimiter="\t")
+
 data_array = data.view((np.float, len(data.dtype.names)))
 data_array = data_array[1:1000].transpose()
-data_dist = pdist(data_array)
 
+data_dist = pdist(data_array) # computing the distance
 
-data_link = linkage(data_dist)
+data_link = linkage(data_dist) # computing the linkage
 
 # just plot the dendrogram.
 dendrogram(data_link,labels=data.dtype.names)
@@ -18,7 +18,7 @@ plt.savefig('../../results/dendrogram.png')
 # or plot the heatmap too!
 
 # Compute and plot first dendrogram.
-fig = pylab.figure(figsize=(8,8))
+fig = plt.figure(figsize=(8,8))
 # x ywidth height
 ax1 = fig.add_axes([0.05,0.1,0.2,0.6])
 Y = linkage(data_dist, method='single')
@@ -38,12 +38,12 @@ idx2 = Z2['leaves']
 D = squareform(data_dist)
 D = D[idx1,:]
 D = D[:,idx2]
-im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=plt.cm.YlGnBu)
 axmatrix.set_xticks([])
 axmatrix.set_yticks([])
 
 # Plot colorbar.
 axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
-pylab.colorbar(im, cax=axcolor)
+plt.colorbar(im, cax=axcolor)
 
 fig.savefig('../../results/heatmap.png')
